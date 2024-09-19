5d545b4d1a32021a645ef009add3e409b02bd92f
SamuelColmaan <samuelcolman07@gmail.com>
2024-09-19 11:54:23 -0300

    Ao clicar no botão de tenho interesse adiciona ao input o nome do veículo

--- EBAC/main.js
+++ EBAC/main.js
@@ -1,10 +1,13 @@
-$(document).ready(()=>{
-    $('#carousel-images').slick({autoplay:true, arrows: false})
+$(document).ready(function() {
+    $('#carousel-images').slick({
+        autoplay: true,
+        arrows: false
+    });
     // Esse bloco faz com que quando clique no menu hamburguer ele desça um suba
-    $('.menu-hamburguer').click(()=>{
+    $('.menu-hamburguer').click(function() {
         $('nav').slideToggle();
-    })
-    $('#telefone').mask('(00) 00000-0000')
+    });
+    $('#telefone').mask('(00) 00000-0000');
     $('form').validate({
         rules: {
             nome: {
@@ -17,30 +20,32 @@ $(document).ready(()=>{
             telefone: {
                 required: true
             },
-            mensagem:{
+            mensagem: {
                 required: true
             },
-            veiculoInteresse:{
+            veiculoInteresse: {
                 required: false
             }
         },
-        messages:{
+        messages: {
             nome: 'Por favor, insira o seu nome',
             email: 'Por favor, insira o seu e-mail',
-            telefone: 'por favor, insira o número de telefone'
+            telefone: 'Por favor, insira o número de telefone'
         },
-        submitHandler: (form)=>{
-            console.log(form)
+        submitHandler: function(form) {
+            console.log(form);
         },
-        invalidHandler: (evento, validador)=>{
+        invalidHandler: function(evento, validador) {
             let camposIncorretos = validador.numberOfInvalids();
-            console.log(camposIncorretos)
+            console.log(camposIncorretos);
         }
-    })
-    $('.lista-veiculos button').click(()=>{
+    });
+    $('.lista-veiculos button').click(function() {
         const destino = $('#contato');
+        const nomeVeiculo = $(this).parent().find('h3').text();
+        $('#veiculoInteresse').val(nomeVeiculo);
         $('html').animate({
             scrollTop: destino.offset().top
-        }, 1000)
-    })
-}); 
+        }, 1000);
+    });
+});
